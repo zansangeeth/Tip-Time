318d02bb7d7727ca8064116f866be1ead4ed5b93
zansangeeth <zansangeeth17@gmail.com>
2021-08-26 18:53:26 +0530

    final commit

--- app/src/main/java/com/zasa/tiptime/MainActivity.kt
+++ app/src/main/java/com/zasa/tiptime/MainActivity.kt
@@ -1,66 +1,40 @@
 package com.zasa.tiptime
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-
-import com.zasa.tiptime.databinding.ActivityMainBinding
-
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{calculateTip()}
-
+        calculate_button.setOnClickListener {
+            calculateTip()
+        }
     }
 
-    private fun calculateTip(){
-        val stringInTextField = binding.costOfServiceEditText.text.toString()
-        val cost = stringInTextField.toDoubleOrNull()
 
-        if (cost == null || cost == 0.0){
-            return displayTip(0.0)
-        }
+    private fun calculateTip() {
+        var costAmount = cost_of_service_edit_text.text.toString()
+        var doubleCost = costAmount.toIntOrNull()
 
-        val tipPercentage = when (binding.tipOption.checkedRadioButtonId){
-            R.id.option_twenty_percent -> 0.20
+        var tipOption = when(tip_option.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.2
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
-
-        var tip =tipPercentage * cost
-
-//        val roundUp = binding.roundUpSwitch.isChecked
-//        if (roundUp) {
-//            // Take the ceiling of the current tip, which rounds up to the next integer, and store
-//            // the new value in the tip variable.
-//            tip = kotlin.math.ceil(tip)
-//        }
-
-        displayTip(tip)
-        Log.d("sample", "tip : $tip")
-
-
+        var calculate = doubleCost!! *tipOption
+        tip_results.setText("$ " + calculate.toString())
 
     }
-
-
-    private fun displayTip(tip: Double) {
-
-        binding.tipResults.text = getString(R.string.tip_amount)
-
-    }
-
 
 
 }
+
+
 
 
 
